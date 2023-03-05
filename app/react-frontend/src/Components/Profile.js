b56c481da9712fdc92c76b113049877a570e6d51
Dillon Rego <dillonjrego17@gmail.com>
2023-03-04 18:23:38 -0800

    Added profile image modifications

--- app/react-frontend/src/Components/Profile.js
+++ app/react-frontend/src/Components/Profile.js
@@ -1,17 +1,28 @@
 import Userfront from "@userfront/react";
-import React from 'react'
+import React, { useState } from 'react';
 
 const Profile = () => {
   const userData = Userfront.user;
+
+  const [link, setLink] = useState(userData.image);
+
+  function updateLink(link)
+  {
+    userData.update({image: link});
+    console.log(link);
+  }
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <div style={{ textAlign: 'center' }}>
         <img
-          src={userData.image}
+          src={link}
           alt="User profile"
           style={{ maxWidth: '100%', marginBottom: '16px' }}
         />
+        <label htmlFor="imglink">Link To Profile Image:</label>
+      <input type="text" id="title" value={link} onChange={d => setLink(d.target.value)}/>
+        <button onClick={ (a) => {updateLink(link);}}>Submit</button>
         <h2>{userData.name}</h2>
         <p>@{userData.username}</p>
         <p>{userData.email}</p>
@@ -19,6 +30,8 @@ const Profile = () => {
       </div>
     </div>
   );
+
 }
+
 
 export default Profile;
