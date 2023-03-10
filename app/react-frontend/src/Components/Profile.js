6d49633032f591817b8ef57b170992cf72019225
Dillon Rego <dillonjrego17@gmail.com>
2023-03-10 13:56:31 -0800

    Update Profile.js

--- app/react-frontend/src/Components/Profile.js
+++ app/react-frontend/src/Components/Profile.js
@@ -1,15 +1,39 @@
 import Userfront from "@userfront/react";
-import React, { useState } from 'react';
+import React, {useState } from 'react';
 
 const Profile = () => {
   const userData = Userfront.user;
-
   const [link, setLink] = useState(userData.image);
+  const [bState, setButton] = useState(false);  
+  const [bText, setBText] = useState("Edit");
+
 
   function updateLink(link)
   {
     userData.update({image: link});
     console.log(link);
+  }
+
+  function toggleShowImageData()
+  {
+    setButton(!bState);
+    if(bText === "Edit")
+      setBText("Close");
+    else
+      setBText("Edit");
+  }
+
+  function SelectiveRender()
+  {
+      if(bState === true)
+      {
+        return(
+        <h2>
+        <label htmlFor="imglink">Link To Profile Image:</label>
+        <input type="text" id="title" value={link} onChange={d => setLink(d.target.value)}/>
+        <button onClick={ (a) => {updateLink(link);}}>Submit</button>
+        </h2>
+      )}
   }
 
   return (
@@ -20,10 +44,11 @@ const Profile = () => {
           alt="User profile"
           style={{width: 200, height: 200, margin: 5}}
         />
-        <label htmlFor="imglink">Link To Profile Image:</label>
-      <input type="text" id="title" value={link} onChange={d => setLink(d.target.value)}/>
-        <button onClick={ (a) => {updateLink(link);}}>Submit</button>
-        <h2>{userData.name}</h2>
+        <h2>
+        <button onClick={ (a) => {toggleShowImageData();}}>{bText}</button>
+        <SelectiveRender></SelectiveRender>
+        </h2>
+        <h3>{userData.name}</h3>
         <p>@{userData.username}</p>
         <p>{userData.email}</p>
         <button onClick={Userfront.logout}>Logout</button>
